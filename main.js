17ea193dcc2ad7800c356bf1ee10bedd85c23c18
nakahararuu <repika02@gmail.com>
2021-08-04 19:48:48 +0900

    init commit

---
+++ main.js
@@ -0,0 +1,40 @@
+const { chromium } = require('playwright');
+
+(async () => {
+	const browser = await chromium.launch()
+	const page = await browser.newPage()
+	const navigationPromise = page.waitForNavigation()
+	
+  	await page.goto(`https://www.pixiv.net/?return_to=%2Fusers%2F${process.env.PIXIV_USER_ID}%2Fbookmarks%2Fartworks`);
+	await page.setViewportSize({ width: 1280, height: 696 })
+	await navigationPromise
+
+	// ログイン
+	await page.click('text=ログイン')
+	await page.fill('text=ログインパスワードがわからない >> [placeholder="メールアドレス / pixiv ID"]', process.env.PIXIV_LOGIN_ID);
+	await page.fill('text=ログインパスワードがわからない >> [placeholder="パスワード"]', process.env.PIXIV_PASSWORD);
+	await Promise.all([
+		page.waitForNavigation(/*{ url: 'https://www.pixiv.net/users/192633/bookmarks/artworks' }*/),
+		page.click('#LoginComponent >> text=ログイン')
+	]);
+	
+	// ブックマークの編集を開始
+	await page.waitForSelector('text=ブックマーク管理')
+	await page.click('text=ブックマーク管理')
+	
+	// R-18のものをすべてクリック
+	await page.waitForSelector('text=R-18')
+	const targets = await page.$$('text=R-18').forEach(e => e.click())
+	
+	// 非公開に設定
+	await page.waitForSelector('text=非公開にする')
+	await page.click('text=非公開にする')
+
+	await browser.close()
+})();
+
+
+
+
+
+
