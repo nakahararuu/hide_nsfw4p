5b78734c6bf0aad373dd4d94ac2d38655ac266b3
nakahararuu <repika02@gmail.com>
2021-08-05 13:48:47 +0900

    refactor: add semicolon

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 const { chromium } = require('playwright');
 
 (async () => {
-	const browser = await chromium.launch()
-	const page = await browser.newPage()
-	const navigationPromise = page.waitForNavigation()
+	const browser = await chromium.launch();
+	const page = await browser.newPage();
+	const navigationPromise = page.waitForNavigation();
 	
   	await page.goto(`https://www.pixiv.net/?return_to=%2Fusers%2F${process.env.PIXIV_USER_ID}%2Fbookmarks%2Fartworks`);
-	await page.setViewportSize({ width: 1280, height: 696 })
-	await navigationPromise
+	await page.setViewportSize({ width: 1280, height: 696 });
+	await navigationPromise;
 
 	// ログイン
-	await page.click('text=ログイン')
+	await page.click('text=ログイン');
 	await page.fill('text=ログインパスワードがわからない >> [placeholder="メールアドレス / pixiv ID"]', process.env.PIXIV_LOGIN_ID);
 	await page.fill('text=ログインパスワードがわからない >> [placeholder="パスワード"]', process.env.PIXIV_PASSWORD);
 	await Promise.all([
@@ -19,17 +19,17 @@ const { chromium } = require('playwright');
 	]);
 	
 	// ブックマークの編集を開始
-	await page.waitForSelector('text=ブックマーク管理')
-	await page.click('text=ブックマーク管理')
+	await page.waitForSelector('text=ブックマーク管理');
+	await page.click('text=ブックマーク管理');
 	
 	// R-18のものをすべてクリック
-	await page.waitForSelector('text=R-18')
-	await page.$$eval('text=R-18', elements => elements.forEach(e => e.click()))
+	await page.waitForSelector('text=R-18');
+	await page.$$eval('text=R-18', elements => elements.forEach(e => e.click()));
 	
 	// 非公開に設定
-	await page.waitForSelector('div[role="button"]:has-text("非公開にする")')
+	await page.waitForSelector('div[role="button"]:has-text("非公開にする")');
 	await page.click('div[role="button"]:has-text("非公開にする")');
 
-	await browser.close()
+	await browser.close();
 })();
 
