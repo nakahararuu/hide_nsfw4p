27d77d5a47ac0111867b892e5c2401e60759c0a9
nakahararuu <repika02@gmail.com>
2021-08-05 16:26:36 +0900

    feat: handle a case that there are no NSFW aetworks

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 const { chromium } = require('playwright');
 const { existsSync } = require('fs');
 
-async function openBookmarkPage() {
+async function openBookmarkPage(browser) {
 	const authenticationStateFilePath = '.state/state.json';
 	const hasAuthenticationState = existsSync(authenticationStateFilePath)
 
-	const browser = await chromium.launch();
 	const context = await browser.newContext(hasAuthenticationState ? { storageState: authenticationStateFilePath } : {});
 	const page = await context.newPage();
 	const navigationPromise = page.waitForNavigation();
@@ -28,16 +27,26 @@ async function openBookmarkPage() {
 
 	return page;
 }
- 
+
+async function hasNsfwArtworks(page) {
+	const nsfwArtworks= await page.$$('text=R-18');
+	return nsfwArtworks.length > 0;
+}
+
 (async () => {
-	const page = await openBookmarkPage();
+	const browser = await chromium.launch();
+	const page = await openBookmarkPage(browser);
+
+	if(!(await hasNsfwArtworks(page))){
+		await browser.close();
+		return;
+	}
 
 	// ブックマークの編集を開始
 	await page.waitForSelector('text=ブックマーク管理');
 	await page.click('text=ブックマーク管理');
 	
 	// R-18のものをすべてクリック
-	await page.waitForSelector('text=R-18');
 	await page.$$eval('text=R-18', elements => elements.forEach(e => e.click()));
 	
 	// 非公開に設定
