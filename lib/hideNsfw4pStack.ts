4e6818f71dc49231ecf1a261238cbdae3ae14b96
nakahararuu <repika02@gmail.com>
2022-01-17 21:44:34 +0900

    format code

--- lib/hideNsfw4pStack.ts
+++ lib/hideNsfw4pStack.ts
@@ -6,33 +6,33 @@ import cdk = require('@aws-cdk/core');
 import ssm = require('@aws-cdk/aws-ssm');
 
 export class HideNsfw4pStack extends cdk.Stack {
-  constructor(app: cdk.App, id: string) {
-    super(app, id);
+	constructor(app: cdk.App, id: string) {
+		super(app, id);
 
-    const bucket = new s3.Bucket(this, 'MyBucket', { 'lifecycleRules': [{expiration: cdk.Duration.days(20)}] });
+		const bucket = new s3.Bucket(this, 'MyBucket', { 'lifecycleRules': [{expiration: cdk.Duration.days(20)}] });
 
-    const lambdaFn = new lambda.DockerImageFunction(this, 'Singleton', {
-	    code: lambda.DockerImageCode.fromImageAsset('src/', {
-		    cmd: [ "entry-point/lambda-handler.handler" ],
-		    entrypoint: ["/lambda-entrypoint.sh"]
-	    }),
-	    retryAttempts: 0,
-	    memorySize: 1800,
-	    timeout: cdk.Duration.seconds(300),
-	    environment: {
-		    PIXIV_LOGIN_ID: ssm.StringParameter.valueForStringParameter(this, '/hide_nsfw4p/pixiv_login_id'),
-		    PIXIV_PASSWORD: ssm.StringParameter.valueForStringParameter(this, '/hide_nsfw4p/pixiv_login_password'),
-		    PIXIV_USER_ID: ssm.StringParameter.valueForStringParameter(this, '/hide_nsfw4p/pixiv_user_id'),
-		    BUCKET_NAME: bucket.bucketName
-	    }
-    });
-    bucket.grantReadWrite(lambdaFn);
+		const lambdaFn = new lambda.DockerImageFunction(this, 'Singleton', {
+			code: lambda.DockerImageCode.fromImageAsset('src/', {
+				cmd: [ "entry-point/lambda-handler.handler" ],
+				entrypoint: ["/lambda-entrypoint.sh"]
+			}),
+			retryAttempts: 0,
+			memorySize: 1800,
+			timeout: cdk.Duration.seconds(300),
+			environment: {
+				PIXIV_LOGIN_ID: ssm.StringParameter.valueForStringParameter(this, '/hide_nsfw4p/pixiv_login_id'),
+				PIXIV_PASSWORD: ssm.StringParameter.valueForStringParameter(this, '/hide_nsfw4p/pixiv_login_password'),
+				PIXIV_USER_ID: ssm.StringParameter.valueForStringParameter(this, '/hide_nsfw4p/pixiv_user_id'),
+				BUCKET_NAME: bucket.bucketName
+			}
+		});
+		bucket.grantReadWrite(lambdaFn);
 
-    const rule = new events.Rule(this, 'Rule', {
-      schedule: events.Schedule.rate(cdk.Duration.hours(5))
-    });
+		const rule = new events.Rule(this, 'Rule', {
+		  schedule: events.Schedule.rate(cdk.Duration.hours(5))
+		});
 
-    rule.addTarget(new targets.LambdaFunction(lambdaFn));
-  }
+		rule.addTarget(new targets.LambdaFunction(lambdaFn));
+	}
 }
 
